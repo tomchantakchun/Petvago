887cdc83cf700f2888ec356f72a977449b5633f6
ynmwong <melodysweet@gmail.com>
2019-01-17 13:09:08 +0800

    added user route jwt and hotel backend get all info

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,23 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-async function jwtVerify(req, res){
-  return new Promise((resolve,reject)=>{
-      var auth_header = req.headers.authorization.split(" ");
-
-      if(auth_header[0] == "Bearer"){
-         jwt.verify(auth_header[1],process.env.SECRET,function(err, decoded) {
-              if (err) return res.status(500).send({ status: "failed", error: "System error!"})
-              resolve(decoded);
-          });
-      }else{
-          resolve(null);
-      }
-  });     // End of new Promise
-}
-
 // Get user profile information: rows = an array with an object
-router.get('/:id', function(req, res, next) {
+router.get('/:id', async function(req, res, next) {
+  console.log(req.user)
+
+
+
   var db=req.db;
   let query=db.select("*").from("users").where("id",req.params.id)
   query.then((rows)=>{
