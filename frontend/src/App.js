e2d377bb650894e8d647eba2f9c9c44d31df4e8a
zwings <zwings@github.com>
2019-01-21 14:31:38 +0800

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './component/Login/Login'
 import User from './component/User/User'
 import AuthenticatedComponent from './component/Login/AuthenticatedComponent'
+import Navigationbar from './component/Navbar/Navbar'
 
 const Logged = (props) => {
   return (
@@ -14,17 +15,21 @@ const Logged = (props) => {
 class App extends Component {
   render() {
     return (
+      
+      <div><Navigationbar/>
       <Router>
         <div className="App">
           <Switch>
             <Route exact path='/login' component={Login} />
             <Route exact path='/user' component={User} />
+            <Route exact path='/' component={Logged} />
             <AuthenticatedComponent>
-              <Route exact path='/' component={Logged} />
+              
             </AuthenticatedComponent>
           </Switch>
         </div>
       </Router>
+      </div>
     );
   }
 }
