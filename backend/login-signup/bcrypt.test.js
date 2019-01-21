79bfc70afbc6e97766507fd985fdbb6a6ab10524
Matthew Mak <makhoulong@yahoo.com.hk>
2019-01-21 18:07:39 +0800

    Added google map component

--- backend/login-signup/bcrypt.test.js
+++ backend/login-signup/bcrypt.test.js
@@ -7,4 +7,10 @@ describe('Bcrypt module: ', () => {
         const compareResult = await bcypt.checkPassword('123456',hashedPassword)
         expect(compareResult).toEqual(true); 
    }); 
+
+   it('Bcrypt password abcd and compare the hash', async () => {
+      const hashedPassword = await bcypt.hashPassword('abcd')
+      const compareResult = await bcypt.checkPassword('abcde',hashedPassword)
+      expect(compareResult).toEqual(false); 
+   }); 
 });
