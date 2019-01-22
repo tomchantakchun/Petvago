d54528f012bcc92feef7299834be530adba8895f
Tom Chan <ittomchan@gmail.com>
2019-01-22 16:51:00 +0800

    json file access

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -5,20 +5,20 @@ router.post('/', function(req, res, next) {
 
  var db=req.db;
 
-  let query=db.select('booking.hotelID', "booking.roomTypeID").from("hotel")
+  let query=db.select("*").from("hotel")
   .where("district",req.body.district) //search by district
   .innerJoin("roomType", 'hotel.id', "roomType.hotelID") //match roomtype with hotel
-  .where("roomType.requirement.pet", req.body.petType)
-  .innerJoin('booking', function () {
-    this
-      .on("hotel.id", "booking.hotelID")
-      .on("roomType.id", 'booking.roomTypeID');
-  }) //find booking related
-  .where(function() {
-    this.where("booking.endDate", '<=', req.body.startDate).orWhere("booking.startDate", '>=', req.body.endDate)
-  })
+  .whereRaw("requirement ->> 'pet' = ?", [req.body.petType])
+  // .innerJoin('booking', function () {
+  //   this
+  //     .on("hotel.id", "booking.hotelID")
+  //     .on("roomType.id", 'booking.roomTypeID');
+  // }) //find booking related
+  // .where(function() {
+  //   this.where("booking.endDate", '<=', req.body.startDate).orWhere("booking.startDate", '>=', req.body.endDate)
+  // })
 
-  query.then((rows)=>{
+   query.then((rows)=>{
 
     res.send(rows);
   })
