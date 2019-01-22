b726d069c9c2ffbe5dd4723291d7f2c842fc240e
Tom Chan <ittomchan@gmail.com>
2019-01-22 19:31:36 +0800

    testing timezone problem

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -8,19 +8,24 @@ router.post('/', function(req, res, next) {
   let query=db.select("*").from("hotel")
   .where("district",req.body.district) //search by district
   .innerJoin("roomType", 'hotel.id', "roomType.hotelID") //match roomtype with hotel
-  .whereRaw("requirement ->> 'pet' = ?", [req.body.petType])
-  // .innerJoin('booking', function () {
-  //   this
-  //     .on("hotel.id", "booking.hotelID")
-  //     .on("roomType.id", 'booking.roomTypeID');
-  // }) //find booking related
+  .innerJoin('booking', function () {
+    this
+      .on("hotel.id", "booking.hotelID")
+      .on("roomType.id", 'booking.roomTypeID');
+  }) //find booking related
   // .where(function() {
   //   this.where("booking.endDate", '<=', req.body.startDate).orWhere("booking.startDate", '>=', req.body.endDate)
-  // })
-
+  // }) 
+  .whereRaw("requirement ->> 'pet' = ?", [req.body.petType]) //search by petType
+  
    query.then((rows)=>{
-
-    res.send(rows);
+     res.send(rows)
+    // rows.map((row, index) => {
+    //   let hotelID = row.hotelID;
+    //   let roomTypeID = row.roomTypeID;
+    //   let query2=db.select("quantity").from("roomType").where('hotelID', hotelID).andWhere('id', roomTypeID)
+    //   query2.then((results)=>{res.send(results)})
+    // })
   })
   
   .catch((error)=>{
--- frontend/src/component/Search/Search.js
+++ frontend/src/component/Search/Search.js
@@ -29,7 +29,7 @@ class Search extends React.Component {
                 } else {
                     console.log(response.data)
                     for (let i=0; i < response.data.length; i++){
-                    console.log(response.data[i])
+                    console.log(response.data[i].startDate)
                     };
                 }
             this.props.onSearch(this.state);
