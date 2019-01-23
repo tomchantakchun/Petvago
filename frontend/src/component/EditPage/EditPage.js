90e2f4e3040a8e1380d865c6e47daee33cc57576
Matthew Mak <makhoulong@yahoo.com.hk>
2019-01-23 21:20:52 +0800

    Finished styling edit hotel page

--- frontend/src/component/EditPage/EditPage.js
+++ frontend/src/component/EditPage/EditPage.js
@@ -4,8 +4,9 @@ import './EditPage.css'
 // Setting up Fontawesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { faPlusSquare, faEdit } from '@fortawesome/free-solid-svg-icons'
 library.add(faPlusSquare)
+library.add(faEdit)
 
 class EditPage extends React.Component {
     constructor (props) {
@@ -45,47 +46,62 @@ class EditPage extends React.Component {
                                     </tr>
                                     <tr>
                                         <td><label>Tel: </label></td>
-                                        <td><input type='text' name='hotelTel' value={this.state.hotelTel} onChange={this.handleInputChange}></input></td>
+                                        <td><input type='text' name='hotelTel' value={this.state.hotelTel} onChange={this.handleInputChange} className='inputField'></input></td>
                                     </tr>
                                     <tr>
                                         <td><label>Address: </label></td>
-                                        <td><input type='text' name='hotelAddress' value={this.state.hotelAddress} onChange={this.handleInputChange}></input></td>
+                                        <td><input type='text' name='hotelAddress' value={this.state.hotelAddress} onChange={this.handleInputChange} className='inputField'></input></td>
                                     </tr>
                                     <tr>
                                         <td><label>Description: </label></td>
-                                        <td><input type='text' name='hotelDescription' value={this.state.hotelDescription} onChange={this.handleInputChange}></input><br/></td>
+                                        <td><input type='text' name='hotelDescription' value={this.state.hotelDescription} onChange={this.handleInputChange} className='inputField'></input><br/></td>
                                     </tr>
                                 </tbody>
                             </table>
-                            <div id='HotelIcon'></div>
+                            <div id='HotelIcon'>
+                                <FontAwesomeIcon icon="edit" />
+                            </div>
                         </div>
                         
-
-                        <label>Room Types:</label>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th>Type</th>
-                                    <th>Price</th>
-                                    <th>Edit</th>
-                                </tr>
-                                {this.state.roomType}
-                                <tr>
-                                    <td><FontAwesomeIcon icon="plus-square" /></td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div id='EditHotelRoomType'>
+                            <h4>Room Types:</h4>
+                            <table className='table table-hover table-bordered'>
+                                <thead className='thead-dark'>
+                                    <tr>
+                                        <th>Type</th>
+                                        <th>Price</th>
+                                        <th>Edit</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.roomType}
+                                    <tr className='table-secondary'>
+                                        <td></td>
+                                        <td className='plusSquare'><FontAwesomeIcon icon="plus-square" /></td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                         
-                        <label>Vaccine Requirement</label>
-                        <input type='checkbox' name='RabiesVaccination' value='RabiesVaccination'/>
-                        <p>Rabies Vaccination</p>
-                        <input type='checkbox' name='DHPPiLVaccination' value='DHPPiLVaccination'/>
-                        <p>DHPPiL Vaccination</p>
-                        <input type='checkbox' name='FVRCPVaccination' value='FVRCPVaccination'/>
-                        <p>FVRCP Vaccination</p>
+                        <h4>Vaccine Requirement</h4>
+                        <div className='custom-control custom-checkbox'>
+                            <input type='checkbox' name='RabiesVaccination' value='RabiesVaccination' id='RabiesVaccination' className="custom-control-input"/>
+                            <label className="custom-control-label" for="RabiesVaccination">Rabies Vaccination</label>
+                        </div>
+                        <div className='custom-control custom-checkbox'>
+                            <input type='checkbox' name='DHPPiLVaccination' value='DHPPiLVaccination' id='DHPPiLVaccination' className="custom-control-input"/>
+                            <label className="custom-control-label" for="DHPPiLVaccination">DHPPiL Vaccination</label>
+                        </div>
+                        <div className='custom-control custom-checkbox'>
+                            <input type='checkbox' name='FVRCPVaccination' value='FVRCPVaccination' id='FVRCPVaccination' className="custom-control-input"/>
+                            <label className="custom-control-label" for="FVRCPVaccination">FVRCP Vaccination</label>
+                        </div>
 
-                        <input type='submit' value='Upadate'></input>
-                        <button onClick={this.handleCancel}>Cancel</button>
+                        <div id='FormFooter'>
+                            <input type='submit' value='Upadate' className='btn btn-success btn-lg px-5'></input>
+                            <button onClick={this.handleCancel} className='btn btn-lg px-5'>Cancel</button>
+                        </div>
 
                     </form>
                 </section>
