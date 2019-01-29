bb2efe7c8f26b3c5ca553fea41898c4c69dd9ac9
Matthew Mak <makhoulong@yahoo.com.hk>
2019-01-29 15:37:35 +0800

    setup table for booking management

--- frontend/src/component/HostManagement/HostMangement.js
+++ frontend/src/component/HostManagement/HostMangement.js
@@ -2,18 +2,25 @@ import React from 'react';
 import './HostManagement.css'
 
 class HostManagement extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
-            
+
         }
+        this.rooms = null;
     }
 
     handleEditDetail = () => {
         this.props.history.push('/edit-hotel-info')
     }
 
-    render () {
+    componentDidMount() {
+
+    }
+
+    render() {
+
+
         return (
             <div id='HostManagement'>
                 <h2>Hotel Name: </h2>
@@ -22,6 +29,25 @@ class HostManagement extends React.Component {
                     <button type="button" class="btn btn-outline-secondary" onClick={this.handleEditDetail}>Edit</button>
                 </div>
                 <h2>Booking Management</h2>
+                <div className='booking-management'>
+                    <table className='table table-hover table-bordered'>
+                        <thead className='thead-dark'>
+                            <tr>
+                                <th className=''></th>
+                                <th className='th-dow'>Sun</th>
+                                <th className='th-dow'>Mon</th>
+                                <th className='th-dow'>Tue</th>
+                                <th className='th-dow'>Wed</th>
+                                <th className='th-dow'>Thu</th>
+                                <th className='th-dow'>Fri</th>
+                                <th className='th-dow'>Sat</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.rooms}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
