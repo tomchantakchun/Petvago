ef219ea01bae5347f5894dc8046eaeda4a9aff59
ynmwong <melodysweet@gmail.com>
2019-01-31 17:24:56 +0800

    fix search result sorter filter css

--- frontend/src/component/Sorter/Sorter.js
+++ frontend/src/component/Sorter/Sorter.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import './Sorter.css'
 import * as actionTypes from '../../store/actions';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown, faAngleUp, faDollarSign, faStar } from '@fortawesome/free-solid-svg-icons';
+library.add(faAngleDown)
+library.add(faAngleUp)
+library.add(faDollarSign)
+library.add(faStar)
+
+
+
 
 const mapStateToProps = state => {
     return {
@@ -26,26 +37,26 @@ class Sorter extends React.Component{
         let nameButton, priceButton, ratingButton;
       
         if (this.props.SearchResult.sortPeference === "NameAscending"){
-            nameButton = <button className="lightgreen" onClick={()=>this.onSort('NameDescending')}>Name A-Z </button>;
+            nameButton = <button className="btn sort-button" style={{color:'#fff',backgroundColor:'#50b5a9'}} onClick={()=>this.onSort('NameDescending')}>Name <FontAwesomeIcon icon="angle-up" style={{marginLeft:'5px'}}/> </button>;
         } else if (this.props.SearchResult.sortPeference === "NameDescending") {
-            nameButton = <button className="lightgreen" onClick={()=>this.onSort('NameAscending')}>Name Z-A</button>;
+            nameButton = <button className="btn sort-button" style={{color:'#fff',backgroundColor:'#50b5a9'}}  onClick={()=>this.onSort('NameAscending')}>Name <FontAwesomeIcon icon="angle-down" style={{marginLeft:'5px'}}/></button>;
         } else {
-            nameButton = <button className="grey" onClick={()=>this.onSort('NameAscending')}>Name</button>;
+            nameButton = <button className="btn sort-button-grey sort-button" onClick={()=>this.onSort('NameAscending')}>Name</button>;
         }
         if (this.props.SearchResult.sortPeference === "PriceAscending"){
-            priceButton = <button className="lightgreen" onClick={()=>this.onSort('PriceDescending')}>Price: Low to High </button>;
+            priceButton = <button className="btn sort-button" style={{color:'#fff',backgroundColor:'#50b5a9'}}  onClick={()=>this.onSort('PriceDescending')}><FontAwesomeIcon icon="dollar-sign" style={{marginRight:'5px'}}/>Price <FontAwesomeIcon icon="angle-up" style={{marginLeft:'5px'}}/></button>;
         } else if (this.props.SearchResult.sortPeference === "PriceDescending") {
-            priceButton = <button className="lightgreen" onClick={()=>this.onSort('PriceAscending')}>Price: High to Low</button>;
+            priceButton = <button className="btn sort-button" style={{color:'#fff',backgroundColor:'#50b5a9'}}  onClick={()=>this.onSort('PriceAscending')}><FontAwesomeIcon icon="dollar-sign" style={{marginRight:'5px'}}/>Price <FontAwesomeIcon icon="angle-down" style={{marginLeft:'5px'}}/></button>;
         } else {
-            priceButton = <button className="grey" onClick={()=>this.onSort('PriceAscending')}>Price</button>;
+            priceButton = <button className="btn sort-button-grey sort-button" onClick={()=>this.onSort('PriceAscending')}><FontAwesomeIcon icon="dollar-sign" style={{marginRight:'5px',color:'#50b5a9'}}/>Price</button>;
         }
        
         if (this.props.SearchResult.sortPeference === "RatingAscending"){
-            ratingButton = <button className="lightgreen" onClick={()=>this.onSort('RatingDescending')}>Rating: Low to High </button>;
+            ratingButton = <button className="btn sort-button" style={{color:'#fff',backgroundColor:'#50b5a9'}}  onClick={()=>this.onSort('RatingDescending')}><FontAwesomeIcon icon="star" style={{marginRight:'5px'}}/>Rating <FontAwesomeIcon icon="angle-up" style={{marginLeft:'5px'}}/> </button>;
         } else if (this.props.SearchResult.sortPeference === "RatingDescending") {
-            ratingButton = <button className="lightgreen" onClick={()=>this.onSort('RatingAscending')}>Rating: High to Low</button>;
+            ratingButton = <button className="btn sort-button " style={{color:'#fff',backgroundColor:'#50b5a9'}}  onClick={()=>this.onSort('RatingAscending')}><FontAwesomeIcon icon="star" style={{marginRight:'5px'}}/>Rating <FontAwesomeIcon icon="angle-down" style={{marginLeft:'5px'}}/></button>;
         } else {
-            ratingButton = <button className="grey" onClick={()=>this.onSort('RatingDescending')}>Rating</button>;
+            ratingButton = <button className="btn sort-button-grey sort-button" onClick={()=>this.onSort('RatingDescending')}><FontAwesomeIcon icon="star" style={{marginRight:'5px',color:'#50b5a9'}}/>Rating</button>;
         }
         
         //end of Buttons
